e84312fc462d7f08ed3825294f8274b70803d364
arbyl <aflara123@gmail.com>
2024-01-24 13:43:30 -0500

    set github pages conf

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,11 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
+
+const LIVE_URL = "https://Arbyl.github.io";
+
 // https://astro.build/config
 export default defineConfig({
+  site: LIVE_URL,
   integrations: [tailwind()]
 });
