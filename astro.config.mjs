b535b1055aec99864d7909c003ef4bdfe93d0cff
arbyl <aflara123@gmail.com>
2024-02-20 13:04:10 -0500

    Delete unused files and update demo titles

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
-import { Link } from 'astro/components';
-
 const LIVE_URL = "https://Arbyl.github.io";
 
 // https://astro.build/config
